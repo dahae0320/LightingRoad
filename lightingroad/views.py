99ebbed4015b618b1efde5b4b21b1415163cb2fe
dahae0320 <ldh990320ldh@gmail.com>
2021-07-25 21:21:00 +0900

    jus~~t

--- lightingroad/views.py
+++ lightingroad/views.py
@@ -6,7 +6,7 @@ from urllib.request import urlopen, Request
 from urllib.parse import urlencode, quote_plus, unquote
 
 # Create your views here.
-def streetLampData(lat, lng):
+def streetLampData(add):
   url = 'http://api.data.go.kr/openapi/tn_pubr_public_scrty_lmp_api'
 
   queryParams = '?' + urlencode({ quote_plus('serviceKey') : unquote('n9Pfhnwdrxh%2FiMJefGgPTp2AqXB6JERmRhzBvdbZHL7Cbneqc7N5j6TxUvNOis9Ri%2Fz0dFdM8jbOYhKcmWj2Qg%3D%3D'),
@@ -14,13 +14,14 @@ def streetLampData(lat, lng):
       quote_plus('numOfRows') : '100',
       # quote_plus('latitude') : lat,
       # quote_plus('longitude') : lng,
+      quote_plus('lnmadr') : '서울특별시 서초구 서초동 1666-4',
       quote_plus('type') : 'json'}, encoding='UTF-8', doseq=True)
 
   request = Request(url + queryParams)
   request.get_method = lambda: 'GET'
   response_body = urlopen(request).read()
   decode_data = response_body.decode('utf-8')
-  # print(decode_data)
+  print(decode_data)
   return decode_data
 
 def mapcenter(request):
@@ -31,9 +32,10 @@ def mapcenter(request):
 
 def main(request):  
   if request.is_ajax and request.method == 'POST':
-    lat = request.POST['lat']
-    lng = request.POST['lng']
-    data = streetLampData(lat, lng)      
+    # lat = request.POST['lat']
+    # lng = request.POST['lng']
+    add = request.POST['add']
+    data = streetLampData(add)      
     return HttpResponse(data)
   return render(request, 'main.html')
 
