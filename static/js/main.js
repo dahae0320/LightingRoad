217b1cbc88d7449f5c4cdc1032305dda4111a240
cho <joe217@likelion.org>
2021-08-06 00:55:39 +0900

    머지용

--- static/js/main.js
+++ static/js/main.js
@@ -39,14 +39,13 @@ let passList = []
 function startFn(lat, lng) {
   s_mk_lat = lat;
   s_mk_lng = lng;
-  Pass = '';
 }
 
 function passFn(lat, lng) {
   passList.push(lat)
   passList.push(lng)
   console.log(passList)
-  if (passList.length == 10) {
+  if (passList.length == 11) {
     Pass = ''
     passList = []
   } else {
@@ -176,34 +175,29 @@ function initTmap() {
       marker.addListener('click', (evt) => {
         markerEvent(marker._marker_data.options.title, resultData);
 
-        let content =
-          "<div class='outside' style=' position: relative;  width:150px; border-bottom: 1px solid black; line-height: 18px; padding: 0 35px 2px 0;'>" +
-          "<div class='a' width:130px; style='font-size: 12px; line-height: 15px;'>" +
-          "<span class='b' style='display: inline-block; width:130px; height: 14px; margin-left:2px; vertical-align: middle; margin-right: 5px;'><a href='javascript:void(0);' onclick='startFn(" + marker._marker_data.options.position._lat + "," + marker._marker_data.options.position._lng + "); onClose();'>여기를 출발지로 지정</a></span>" +
-          "</div>" +
-          "</div>" +
-          "<div class='outside' style=' position: relative;  width:150px; border-bottom: 1px solid black; line-height: 18px; padding: 0 35px 2px 0;'>" +
-          "<div class='a' width:130px; style='font-size: 12px; line-height: 15px;'>" +
-          "<span class='b' style='display: inline-block; width:130px; height: 14px; margin-left:2px; vertical-align: middle; margin-right: 5px;'><a href='javascript:void(0);' onclick='passFn(" + marker._marker_data.options.position._lat + "," + marker._marker_data.options.position._lng + "); onClose();'>여기를 경유지로 지정</a></span>" +
-          "</div>" +
-          "</div>" +
-          "<div class='outside' style=' position: relative;  width:150px; line-height: 18px; padding: 0 35px 2px 0;'>" +
-          "<div class='a' width:130px; style='font-size: 12px; line-height: 15px;'>" +
-          "<span class='b' style='display: inline-block; width:130px; height: 14px; margin-left:2px; vertical-align: middle; margin-right: 5px;'><a href='javascript:void(0);' onclick='destinationFn(" + marker._marker_data.options.position._lat + "," + marker._marker_data.options.position._lng + "); onClose();'>여기를 목적지로 지정</a></span>" +
-          "</div>" +
-          "</div>" +
-
-          //JS에서 받아온거가 html에서 못알아먹을수도 있다! 긍까 반드시 개발자모드로 가서 element에서 html에서 잘 인식하는지 확인을 하고 아니다 하면 저기 "+ㅇㅇ+"처럼하기        
-          "</div>" +
-          "</div>";
+        let content ="<div class='info_container' style='position: static; display: flex; flex-direction: column; font-size: 18px; box-shadow: 5px 5px 5px #00000040; border-radius: 10px; top: 410px; left : 800px; width : 170px; background: #FFFFFF 0% 0% no-repeat padding-box;'>"+
+                     "<a class='btn-close' style='position: absolute; top: 5px; right: 5px; display: block; width: 15px; height: 15px; background: url(static/img/x.png) center;' href='javascript:void(0)' onclick='onClose()' ></a>"+
+                     "<div class='info-box'>"+
+                     "<p style='display: block;height: 20px;padding-right:20px;padding-top:5px; padding-left: 15px;font-size: 13px; color: #444;' ><a href='javascript:void(0);' onclick='startFn(" + marker._marker_data.options.position._lat + "," + marker._marker_data.options.position._lng + "); onClose();'>여기를 출발지로 지정</a></p>"+
+                     "<p style='display: block;height: 20px;padding-right:20px; padding-left: 15px;font-size: 13px; color: #444;' ><a href='javascript:void(0);' onclick='passFn(" + marker._marker_data.options.position._lat + "," + marker._marker_data.options.position._lng + "); onClose();'>여기를 경유지로 지정</a></p>"+
+                     "<p style='display: block;height: 20px;padding-right:20px; padding-left: 15px;font-size: 13px; color: #444;' ><a href='javascript:void(0);' onclick='destinationFn(" + marker._marker_data.options.position._lat + "," + marker._marker_data.options.position._lng + "); onClose();'>여기를 목적지로 지정</a></p>"+
+                     "</div>"+
+                     "<a href='javascript:void(0)' onclick='onClose()' class='btn-close' style='position: absolute; top: 10px; right: 10px; display: block; width: 15px; height: 15px; background: url(resources/images/sample/btn-close-w.svg) no-repeat center;'></a>"+
+                     "</div>"+
+                     "</div>";
+        
+          
+        //JS에서 받아온거가 html에서 못알아먹을수도 있다! 긍까 반드시 개발자모드로 가서 element에서 html에서 잘 인식하는지 확인을 하고 아니다 하면 저기 "+ㅇㅇ+"처럼하기       
         //Popup 객체 생성.
 
         infoWindow = new Tmapv2.InfoWindow({
           position: new Tmapv2.LatLng(marker._marker_data.options.position._lat, marker._marker_data.options.position._lng), //Popup 이 표출될 맵 좌표
           content: content, //Popup 표시될 text
           type: 2, //Popup의 type 설정.
+          border :'0px solid #FF0000',
           map: map, //Popup이 표시될 맵 객체
           setVisible: true
+
         });
 
       })
