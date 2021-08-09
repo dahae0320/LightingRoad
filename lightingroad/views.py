c99fe3273fa4cc4ac40729ebf763494d5a999b62
KIM <kdhahtks@naver.com>
2021-08-10 00:57:29 +0900

    JUST DO IT

--- lightingroad/views.py
+++ lightingroad/views.py
@@ -6,21 +6,32 @@ from urllib.request import urlopen, Request
 from urllib.parse import urlencode, quote_plus, unquote
 
 import json
-import requests
+import os, requests
 
 # Create your views here.
 
 def main(request):
+  proxyDict = {
+              "http"  : os.environ.get('FIXIE_URL', ''),
+              "https" : os.environ.get('FIXIE_URL', '')
+            }
+   # r = requests.get('http://www.example.com', proxies=proxyDict)
+
   if request.is_ajax and request.method == 'POST':
     code = request.POST['code']
-    api_data = (requests.get('https://lightingroad-api.herokuapp.com/'+code)).json()
+    api_data = (requests.get('https://lightingroad-api.herokuapp.com/'+code, proxies=proxyDict)).json()
     return HttpResponse(str(api_data))
   return render(request, 'main.html')
 
 def saveAvg(request):
+  proxyDict = {
+              "http"  : os.environ.get('FIXIE_URL', ''),
+              "https" : os.environ.get('FIXIE_URL', '')
+            }
+
   id = request.POST['id']
   print(id)
   avgSum = request.POST['avgSum']
   avgCount = request.POST['avgCount']
-  requests.get('https://lightingroad-api.herokuapp.com/avg/' + id + '/' + avgSum + '/' + avgCount + '/')
+  requests.get('https://lightingroad-api.herokuapp.com/avg/' + id + '/' + avgSum + '/' + avgCount + '/', proxies=proxyDict)
   return HttpResponse("success")
